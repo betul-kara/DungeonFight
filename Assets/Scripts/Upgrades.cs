8139c5a4f2086ff965dd56f1d741d3b9f3fb0459
mehmetkoroglu <mkoroglu334@gmail.com>
2024-03-27 02:33:22 +0300

    item purchase anim added

--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -1,21 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Upgrades : MonoBehaviour
 {
+    [SerializeField] private Button[] buttons;
+
+    [SerializeField] private Color enableColor;
+    [SerializeField] private Color disableColor;
+
+    private void OnEnable()
+    {
+        SetButtonAttributes(true, enableColor);
+    }
+
     public void UpgradeHealth()
     {
         Player.Instance.health += 20;
+        SetButtonAttributes(false, disableColor);
+        buttons[0].GetComponent<Animator>().SetTrigger("Purchased");
     }
 
     public void UpgradeAttack()
     {
-
+        SetButtonAttributes(false, disableColor);
+        buttons[1].GetComponent<Animator>().SetTrigger("Purchased");
     }
 
     public void UpgradeShield()
     {
+        SetButtonAttributes(false, disableColor);
+        buttons[2].GetComponent<Animator>().SetTrigger("Purchased");
+    }
 
+    private void SetButtonAttributes(bool isInteractable, Color color)
+    {
+        foreach (var button in buttons)
+        {
+            button.interactable = isInteractable;
+            button.GetComponent<Image>().color = color;
+        }
     }
 }
